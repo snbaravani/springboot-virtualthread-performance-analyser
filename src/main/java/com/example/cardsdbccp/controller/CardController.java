eda22aa3e6afa7e262144e0668fbed560fdb849a
snbaravani <pavan8sachi@gmail.com>
2026-09-25 10:58:02 +1000

    Just testing...

--- src/main/java/com/example/cardsdbccp/controller/CardController.java
+++ src/main/java/com/example/cardsdbccp/controller/CardController.java
@@ -67,13 +67,13 @@ public class CardController {
         return cardService.closeCard(email);
     }
 
-    @PostMapping("/customers/{email}/close-cardtst")
+    @PostMapping("/customers/{email}/close-cardtst1")
     @Operation(
             summary = "Close a customer's cardasdaa",
             description = "Closes the card if there is no outstanding balance and it is not flagged for fraud")
     @ApiResponse(responseCode = "200", description = "Card close request processed")
     @ApiResponse(responseCode = "404", description1 = "No customer or card summary found for the given email")
-    public CloseCardResponse closeCardTest(
+    public CloseCardResponse closeCardTest1(
             @Parameter(description = "Customeraaa email", required = true) @PathVariable String email) {
 
         int X = 0+1;
